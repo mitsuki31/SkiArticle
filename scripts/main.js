a5b2aaa6a530838c9120da134ed661c4b2025529
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-09-25 11:10:16 +0700

    Remove unused assets and refactor the URL replacer
    * Removed all images in assets, these are no longer used
    * Now the URL replacer will replaces all URLs when the document page is in loading state, and avoiding potential issues

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,64 @@
 /**
  * Copyright (c) 2023 CV. DR2E
  *
- * Version: 1.0, 25 September 2023
+ * Version: 1.15, 25 September 2023
  * Authors: Ryuu Mitsuki, Nuryadani
  */
+ 
 
+/* Execute when the document is in the loading state.
+ * We avoid using `window.onload` to prevent potential issues.
+ *
+ * Additionally, we replace required URLs in the document
+ * while it is in the loading state. This is done to ensure
+ * that users with slower connections do not inadvertently
+ * trigger URL requests before all resources (e.g., images) have loaded.
+ */
+if (document.readyState === "loading") {
+    console.info(`INFO - Document status: ${document.readyState}`);
 
-// Execute immediately when the document has been loaded
-window.onload = (() => {
-    /* Get and parse the JSON file that contains the URLs
-     * required for the web page.
-     */
-    fetch("assets/json/global-urls.json")
-        .then((response) => {
-            // Throw an error if there is a response issue.
-            console.info(`INFO - Response OK: ${response.ok}`);
-            if (!response.ok) {
-                throw new Error("ERROR - Network response was not OK!");
-            }
+    // Define the JSON file containing global URLs
+    const jsonFile = "assets/json/global-urls.json";
 
-            console.info("SUCCESS - 200 - Retrieved data from the JSON file.");
-            return response.json();
-        })
-        .then((jsonData) => {
-            // Extract keys from JSON data
-            const jsonKeys = Object.keys(jsonData);
-            const prefix = "url-";  // Prefix for ID name
+    // Listen for the "DOMContentLoaded" event to safely replace URLs
+    document.addEventListener("DOMContentLoaded", () => {
+        console.info(`INFO - Document status: ${document.readyState}`);
 
-            // Using loop to replace URLs within ID with prefix "url-"
-            for (const key of jsonKeys) {
-                const element = document.getElementById(prefix.concat(key));
-
-                // Check if the element with the constructed ID exists
-                if (element && element.nodeName.toLowerCase() === "a") {
-                    // Replace the URL
-                    element.href = jsonData[key];
+        /* Get and parse the JSON file that contains the URLs
+         * required for the web page.
+         */
+        fetch(jsonFile)
+            .then((response) => {
+                // Throw an error if there is a response issue.
+                console.info(`INFO - Response OK: ${response.ok}`);
+                if (!response.ok) {
+                    throw new Error("ERROR - Network response was not OK!");
                 }
-            }
 
-            console.info("SUCCESS - 200 - All URLs has been replaced.");
-        })
-        .catch((error) => {
-            console.error(`ERROR - ${error}`);
-        })
-})
+                console.info("SUCCESS - Retrieved data from the JSON file.");
+                return response.json();
+            })
+            .then((jsonData) => {
+                // Extract keys from JSON data
+                const jsonKeys = Object.keys(jsonData);
+                const prefix = "url-";  // Prefix for ID name
+
+                // Using loop to replace URLs within ID with prefix "url-"
+                for (const key of jsonKeys) {
+                    const element = document.getElementById(prefix.concat(key));
+
+                    // Check if the element with the constructed ID exists
+                    if (element && element.nodeName.toLowerCase() === "a") {
+                        // Replace the URL
+                        element.href = jsonData[key];
+                    }
+                }
+
+                console.info("SUCCESS - All URLs has been replaced.");
+            })
+            .catch((error) => {
+                console.error(`ERROR - ${error}`);
+            })
+    }, false);
+}
 
