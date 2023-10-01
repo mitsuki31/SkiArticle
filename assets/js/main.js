41ee89fe8ed6ed3f35fe32f644a968b04e7e7f83
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-10-01 14:33:31 +0700

    Organize the project files
    * Separated the CSS setup configurations and variables to a file named `root.css` in the same directory
    * Separated the JavaScript setup jobs to a file named `setup.js` in the same directory
    * Imported all new setup files to `index.html`
    * Refactored the method to fetch the JSON file to asynchronous job, so it will never blocks other jobs to load
    * Some code and documentations improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2023 CV. DR2E
  *
- * Version: 0.16-prototype, 30 September 2023
+ * Version: 0.17-prototype, 1 October 2023
  * Authors: Ryuu Mitsuki, Nuryadani
  */
 
@@ -9,10 +9,7 @@ const navbarMenu = document.getElementById("navbar-menu");
 const navbarButton = document.getElementById("navbar-button");
 const overlay = document.querySelector('.overlay');
 
-// For checking the user's connection status
-const isOnline = navigator.onLine;
-
-console.log(`INFO - Connection status: ${isOnline ? 'ONLINE' : 'OFFLINE'}`);
+console.log(`INFO - Connection status: ${navigator.onLine ? 'ONLINE' : 'OFFLINE'}`);
 
 // Wait for click event on entire elements
 window.addEventListener("click", (event) => {
@@ -31,65 +28,10 @@ window.addEventListener("click", (event) => {
 
 // Toggle navigation menu on click event of navigation button
 navbarButton.addEventListener("click", () => {
-    // Toggle the 'active' class in 'navbar-menu'
+    // Toggle the 'active' class in 'navbar-menu' and 'navbar-button'
     navbarMenu.classList.toggle("active");
     navbarButton.classList.toggle("active");
 
     // Toggle the 'active' class in 'overlay'
     overlay.classList.toggle("active");
 });
-
-
-/* Execute when the document is in the loading state.
- * We avoid using `window.onload` to prevent potential issues.
- *
- * Additionally, we replace required URLs in the document
- * while it is in the loading state. This is done to ensure
- * that users with slower connections do not inadvertently
- * trigger URL requests before all resources (e.g., images) have loaded.
- */
-if (document.readyState === "loading") {
-    // Define the JSON file containing global URLs
-    const jsonFile = "./assets/json/global-urls.json";
-
-    // Listen for the "DOMContentLoaded" event to safely replace URLs
-    document.addEventListener("DOMContentLoaded", () => {
-        /* Get and parse the JSON file that contains the URLs
-         * required for the web page.
-         */
-        fetch(jsonFile)
-            .then((response) => {
-                // Throw an error if there is a response issue.
-                if (!response.ok) {
-                    throw new Error("Network response was not OK!");
-                }
-
-                return response.json();
-            })
-            .then((jsonData) => {
-                // Extract keys from JSON data
-                const jsonKeys = Object.keys(jsonData);
-                const prefix = "dr2e--";  // Prefix for classes' name
-                let totalElements = 0;
-
-                // Using 'for' loop to replace all URLs
-                for (const key of jsonKeys) {
-                    // This variable will be a list, not a single HTML object
-                    const elements = document.getElementsByClassName(prefix.concat(key));
-                    totalElements += elements.length;
-
-                    for (const element of elements) {
-                        if (element &&
-                                element.nodeName.toLowerCase() === 'a') {
-                            element.href = jsonData[key] || "#";
-                        }
-                    }
-                }
-
-                console.info(`SUCCESS - ${totalElements} ${totalElements === 1 ? 'URL' : 'URLs'} has been replaced.`);
-            })
-            .catch((error) => {
-                console.error(`ERROR - ${error}`);
-            })
-    }, false);
-}
---
+++ assets/js/setup.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2023 CV. DR2E
+ *
+ * Version: 1.0-prototype, 1 October 2023
+ * Authors: Ryuu Mitsuki, Nuryadani
+ */
+
+// Path to the JSON file that contains all URLs required by webpage
+// The path relative from 'index.html' (project's root directory)
+const jsonFile = "./assets/json/global-urls.json";
+
+/* Execute when the DOM tree is already loaded (external assets still in loading state).
+ * We avoid using `window.onload` to prevent potential issues.
+ *
+ * Additionally, we replace required URLs in the document
+ * while it is in the loading state. This is done to ensure
+ * that users with slower connections do not inadvertently
+ * trigger URL requests before all resources (e.g., images) have loaded.
+ */
+document.addEventListener("DOMContentLoaded", async () => {
+    /* Get and parse the JSON file that contains the URLs
+     * required for the web page.
+     */
+    await fetch(jsonFile)
+        .then((response) => {
+            // Throw an error if there is a response issue.
+            if (!response.ok) {
+                throw new Error("Network response was not OK!");
+            }
+
+            return response.json();
+        })
+        .then((jsonData) => {
+            // Extract keys from JSON data
+            const jsonKeys = Object.keys(jsonData);
+            const prefix = "dr2e--";  // Prefix for classes' name
+            let totalElements = 0;
+
+            // Using 'for' loop to replace all URLs
+            for (const key of jsonKeys) {
+                // This variable will be a list, not a single HTML object
+                const elements = document.getElementsByClassName(prefix.concat(key));
+                totalElements += elements.length;
+
+                for (const element of elements) {
+                    if (element && element.nodeName === 'A') {
+                        element.href = jsonData[key] || "#";
+                    }
+                }
+            }
+
+            console.info(`SUCCESS - ${totalElements} ${totalElements === 1 ? 'URL' : 'URLs'} has been replaced.`);
+        })
+        .catch((error) => {
+            console.error(`ERROR - ${error}`);
+        })
+});
+
