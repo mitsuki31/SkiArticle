ac8ae9506dfa9df10487c03af058c7c5a6efe2ee
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-10-13 21:38:01 +0700

    Add new function as the main function

--- src/build.js
+++ src/build.js
@@ -42,3 +42,30 @@ const buildSass = (infile, outfile, sassConfig, callback) => {
     }
 }
 
+
+const _run_as_main = () => {
+    const args = process.argv.slice(2);  // Get the command line arguments
+    
+    if (args.length > 0 && args[0].toLowerCase() === 'sass') {
+        const sassFile = path.join(dirpaths.scss, 'main.scss'),
+              outfile = path.join(dirpaths.root, 'public', 'assets', 'css', 'main.css'),
+              parentDir = path.dirname(outfile);
+        
+        // Retrieve and parse the build configuration from 'build.json'
+        const buildConfig = require(path.join(dirpaths.config, 'build.json'));
+        
+        // Check existence of the parent directory, create new directory if not exist
+        if (!fs.existsSync(parentDir)) fs.mkdir(parentDir);
+        
+        buildSass(sassFile, outfile, buildConfig.sass, (err) => {
+            if (err) console.error(err);
+        });
+    }
+}
+
+
+// Main driver
+// Only run when the module is executed directly, not being imported as module
+if (require.main === module) {
+    _run_as_main();
+}
