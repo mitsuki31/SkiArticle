33b603c5601d13d2b0ff1fe4ace6fd4c3dc4c719
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-10-14 21:48:14 +0700

    Refactor the `buildSass` function
    * Imported necessary paths from `utils/coreutils` module
    * Removed the `dirpaths` object, replaced by predefined paths from `utils/coreutils` module
    * Refactored the `buildSass` function and improve the function reliability, robust and errors handling
    * Now the function will create parent directories recursively if not present
    * Added JSDoc documentation to the function

--- src/build.js
+++ src/build.js
@@ -4,41 +4,66 @@ const path = require('path'),              // Path module
       sass = require('sass'),              // Sass module
       config = require('./utils/config');  // Config module (local)
 
-// An object containing necessary paths
-const dirpaths = {
-    root: path.resolve(__dirname, '..'),             // Project root directory path
-    scss: path.resolve(__dirname, 'scss'),           // Sass sources path
-    config: path.resolve(__dirname, '..', 'config')  // Configurations path
-}
+const { rootDir, serverPaths, clientPaths } = require('./utils/coreutils');
 
-// Build and compile specific Sass file and save it to specific file
-// Please note, this function runs synchronously and will lead to blocking some processes
-const buildSass = (infile, outfile, sassConfig, callback) => {
+/**
+ * Builds and compiles a specific Sass file and saves it to a specified
+ * output file.
+ *
+ * <p><b>Note:</b>
+ * This function runs synchronously and may block certain processes.
+ *
+ * @function
+ * @param {!string} infile - Path to the input Sass file to be compiled.
+ * @param {!string} outfile - Path to save the compiled CSS output.
+ * @param {Object} [sassConfig] - Configuration options for Sass compilation.
+ * @param {Function} [callback] - A callback function to handle errors, if any.
+ *
+ * @throws {Error} Throws an error if the input Sass file does not exist
+ *                  or if there are I/O issues.
+ *
+ * @author   Ryuu Mitsuki
+ * @since    0.1.0
+ * @version  1.2
+ */
+function buildSass(infile, outfile, sassConfig, callback) {
     // Resolve and fix the configuration, the configuration will be passed
     // to `sass.compile` arguments
     const resolvedSassConfig = config.resolve(
         "sass", sassConfig, (sassConfig === undefined)
     );
     
-    // Compile the input Sass file
-    const build = sass.compile(
-        (path.isAbsolute(infile) ? infile : path.resolve(infile)),
-        resolvedSassConfig  // Configuration settings
-    );
-    
     // I/O operations
     try {
-        // Check for existence of parent directories
-        // Let users create the directory manually using `fs.mkdir`
-        const parentDir = path.dirname(outfile);
-        if (!fs.existsSync(parentDir)) {
-            throw new Error(`The parent directory is not exist: ${parentDir}`);
+        // Ensure the input Sass file exists
+        if (!fs.existsSync(infile)) {
+            throw new Error(
+                `Given Sass file does not exist: '${infile}'`);
         }
         
-        // Write the compiled CSS to the given output file synchronously
+        // Compile the input Sass file
+        const build = sass.compile(
+            path.resolve(infile),  // Input file
+            resolvedSassConfig     // Configuration settings
+        );
+        
+        // Check for existence of parent directories, create if not present
+        const parentDir = path.dirname(outfile);
+        if (!fs.existsSync(parentDir)) {
+            // Create the directory recursively
+            fs.mkdirSync(parentDir, { recursive: true });
+        }
+        
+        // Write the compiled CSS to the specified output file synchronously
         fs.writeFileSync(path.resolve(outfile), build.css.concat(os.EOL));
     } catch (error) {
-        callback(error);
+        // Handle errors by invoking the provided function, if present,
+        // otherwise throw the errors to outside
+        if (callback && typeof callback === 'function') {
+            callback(error);
+        } else {
+            throw error;
+        }
     }
 }
 
