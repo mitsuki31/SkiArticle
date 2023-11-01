32d5542c47f4e5e3a704c916c2d5edad620d20f7
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-11-01 22:35:53 +0700

    Declare a new typedef for server options
    Separated the options type declaration to make it reusable and
    more readable. This change also added its documentation.

--- src/core/types.d.ts
+++ src/core/types.d.ts
@@ -83,3 +83,26 @@ export interface ServerAddress {
     host: string,
     port: number
 }
+
+/**
+ * Configuration options for the Express server.
+ *
+ * @typedef  {Object} ServerOptions
+ * @property {string|null} [host]
+ *           The host address to bind the server to.
+ * @property {number} [port]
+ *           The port number to listen for incoming requests.
+ *
+ * @author   Ryuu Mitsuki
+ * @since    0.1.0
+ */
+export interface ServerOptions {
+    /**
+     * The host address to bind the server to. If not provided, defaults to `null`.
+     */
+    host?: string | null,
+    /**
+     * The port number to listen for incoming requests. If not provided, defaults to `undefined`.
+     */
+    port?: number
+}
