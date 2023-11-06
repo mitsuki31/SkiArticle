be283a31f8566b3e447aaf1f5bcfe8dbd2dfe749
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-11-06 21:48:14 +0700

    Break all declarations into several modules
    * Removed the `types.d.ts`
    * Moved each declarations to specific module
    * Added `index.d.ts` as main entry declaration file
    * Removed `destination` in SassConfig
    * Refactored the `ResolvedSassConfig` extension to
      omit the `dest` member
    * Added several modules, including:
      - `server.d.ts`
      - `coreutils.d.ts`
      - `config.d.ts`
      - `index.d.ts` (main entry)

---
+++ src/typings/coreutils.d.ts
@@ -0,0 +1,37 @@
+/**
+ * A string representing the directory path.
+ * @global
+ * @typedef {string} StringPath
+ * @author  Ryuu Mitsuki
+ * @since   0.1.0
+ */
+export type StringPath = string;
+
+export interface ClientPaths {
+    root: StringPath,
+    assets: {
+        _this: StringPath,
+        css: StringPath,
+        js: StringPath,
+        images: StringPath
+    },
+    _static: {
+        _this: StringPath,
+        css: StringPath
+    }
+}
+
+export interface ServerPaths {
+    root: StringPath,
+    server: StringPath,
+    scss: StringPath,
+    utils: StringPath,
+    config: StringPath,
+    build: StringPath
+}
+
+export interface LsFilesOptions {
+    match: RegExp,
+    exclude: RegExp,
+    baseName: boolean
+}
---
+++ src/typings/index.d.ts
@@ -0,0 +1,8 @@
+// ** config module ** //
+export * from './config';
+
+// ** coreutil module ** //
+export * from './coreutils';
+
+// ** server module ** //
+export * from './server';
