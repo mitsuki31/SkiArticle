da4513ea70498fc860107e9d737c321e01ee823e
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-11-16 22:58:22 +0700

    Remove the `rootDir` field and use default instead

--- jest.config.js
+++ jest.config.js
@@ -124,7 +124,7 @@ const config = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: 'test',
+  // rootDir: 'test',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
