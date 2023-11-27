7b304e830b7435542e965be75d0789423c491e34
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-11-27 13:28:06 +0700

    Refactor and enhance the `setup` module
    Improved the asynchronous operations and remove the async function
    from promise executor to avoid unexpected behaviour.

--- src/setup.ts
+++ src/setup.ts
@@ -12,47 +12,53 @@ import {
     clientPaths,
     serverPaths,
     lsFiles,
-    copyFile
+    copyFile,
+    exists
 } from './utils/coreutils';
 
 
 export default async function setup(): Promise<void> {
-    return new Promise(async function (
+    return new Promise(function (
         resolve: () => void, reject: (reason?: Error) => void
-    ): Promise<void> {
-        try {
-            // Search for main CSS within 'build' directory
-            const mainCSS: StringPath = (await util.promisify(lsFiles)(
-                serverPaths.build,    // '<rootDir>/build'
-                { match: /\/?main\.css$/, basename: false }
-            ))![0];  // The `lsFiles` returns an array, here we just need the first entry
-            
-            // Throw an error if the main CSS cannot be found
-            if (!mainCSS) {
-                throw new Error("Cannot found 'main.css' file in " +
-                    serverPaths.build + '.\nBuild the CSS file using command: ' +
-                    "'npm run build:css'"
-                );
-            }
-            
-            const outMainCSS: StringPath = path.join(
-                clientPaths.assets.css, path.basename(mainCSS));
-            
-            // Wait till all promises finished their jobs
-            await Promise.all([
-                // Check the existence of "main.css" file
-                fs.promises.access(
-                    mainCSS, fs.constants.F_OK | fs.constants.R_OK),
-                // Create the destination directory
-                fs.promises.mkdir(
-                    path.dirname(outMainCSS), { recursive: true })
-            ]);
-            
-            // Copy the main CSS to client-side
-            await copyFile(mainCSS, outMainCSS);
-        } catch (e: unknown) {
-            if (e instanceof Error) reject(e);
-        }
-        resolve();  // Done
+    ): void {
+        // Search for main CSS within 'build' directory
+        (util.promisify(lsFiles))(serverPaths.build,  // '<rootDir>/build'
+            { match: /\/?main\.css$/, basename: false }
+        )
+            .then(async function (res: Array<StringPath> | null): Promise<void> {
+                // The `lsFiles` returns an array, here we just need the first entry
+                const mainCSS: StringPath = res![0];
+                
+                // Throw an error if the main CSS cannot be found
+                if (!mainCSS && !res?.length) {
+                    reject(new Error("Cannot found 'main.css' file in " +
+                        serverPaths.build + '.\nBuild the CSS file using command: ' +
+                        "'npm run build:css'"
+                    ));
+                    return;  // Exit the function
+                }
+                
+                const outMainCSS: StringPath = path.join(
+                    clientPaths.assets.css, path.basename(mainCSS));
+                
+                try {
+                    await Promise.all([
+                        // Check the existence of "main.css" file
+                        exists(mainCSS),
+                        // Create the destination directory
+                        fs.promises.mkdir(
+                            path.dirname(outMainCSS), { recursive: true })
+                    ]);
+                    
+                    // Copy the main CSS file to client-side
+                    await copyFile(mainCSS, outMainCSS);
+                    resolve();  // Done
+                } catch (e: unknown) {
+                    reject(<Error>e);
+                }
+            })
+            .catch(function (err: Error): void {
+                if (err) reject(err);
+            });
     });
 }
